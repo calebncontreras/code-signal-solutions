0b0dc064fd6965a9f42a9bf38057ef1b50d0c8c1
Caleb <calebncontreras5@gmail.com>
2021-12-30 11:11:34 -0800

    Solution to code signal question "Array Change"

---
+++ array-change.py
@@ -0,0 +1,26 @@
+# Problem: You are given an array of integers. On each move you are allowed to increase exactly one of its element by
+# one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+# Example:
+# For inputArray = [1, 1, 1], the output should be
+# solution(inputArray) = 3.
+
+# Solution -------------------------------------------------------------------------------------------------------------
+
+def solution(a):
+    a_len = len(a)
+    counter = 0
+    for x in range(a_len - 1):
+        while a[x] >= a[x + 1]:
+            a[x + 1] += 1
+            counter += 1
+
+    return counter
+
+
+a = [1, 1, 1]
+b = [-1000, 0, -2, 0]
+c = [2, 1, 10, 1]
+print(solution(a))
+print(solution(b))
+print(solution(c))
